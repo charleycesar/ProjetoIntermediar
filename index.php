5f3e826b452ecf19e1fd29b5eace82035857a4c4
charleycesar@gmail.com <charleycesar@gmail.com>
2014-09-22 10:59:06 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Intermediar-Grupo de mediação escolar</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+
+		<!-- galeria -->
+   <link rel="stylesheet" href="css/themes/default/default.css" type="text/css" media="screen" />
+
+    <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+		<!-- fim galeria -->
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="#Inicio">Inicio</a></li>
+					<li><a href="#Objetivos">Objetivos</a></li>
+					<li><a href="#Equipe">Equipe</a></li>
+					<li><a href="#Galeria">Galeria</a></li>
+					<li><a href="#quem-somos">Quem Somos</a></li>
+					<li><a href="#contact">Contato</a></li>
+				</ul>
+			</nav>
+
+			<!-- index -->
+			<div class="wrapper style1 first">
+				<article class="container" id="Inicio">
+					<div class="row">
+						<div class="4u">
+							<span class="image fit"><img src="images/pic00.jpg" alt="" /></span>
+						</div>
+						<div class="8u">
+							<header>
+								<h1>Apresentação</h1>
+							</header>
+							<p align="justify">
+								&nbsp;&nbsp;&nbsp;Intermediar é um grupo de psicólogos, psicopedagogos, psicomotricistas estudantes e pedagogos, voltados ao acompanhamento da criança que apresente comportamentos que interfiram negativamente em suas aprendizagens e relacionamentos. O acompanhamento ocorre na escola, podendo incluir-se também, os demais espaços sociais e de aprendizagem por onde a criança circula.
+								
+							</p>
+							<p align="justify">
+								&nbsp;&nbsp;&nbsp;O grupo de mediação escolar também oferece consultoria em inclusão, cursos de mediação, equipe de material adaptado, grupo de estudos, aulas particulares, supervisão, recrutamento e seleção de mediadores, indicação para pais e escola, atendimento psicológico, psicopedagógico, acompanhamento terapêutico e grupo na internet para socialização de materiais e estudos. 
+
+							</p>
+							<a href="#Objetivos" class="button big scrolly">Veja Nossos objetivos</a>
+						</div>
+					</div>
+				</article>
+			</div>
+
+			<!-- Objetivos -->
+			<div class="wrapper style2">
+				<article id="Objetivos">
+					<header>
+						<h2>Objetivos</h2>
+						<p align="justify" class="customizarConteudo">
+							Favorecer a vivência, por parte da criança, de experiências intelectuais, afetivas e sociais agradáveis e ricas do ponto de vista do seu desenvolvimento. 
+							Pelo modo com que se dá o trabalho, situando-se entre as relações, pode-se colocar como objetivos complementares: o enriquecimento dos laços entre ela e os pais, e entre ela, as demais crianças e o professor, favorecendo o potencial desses laços influenciarem positivamente nas aprendizagens e no seu desenvolvimento.
+						</p>
+						<p align="justify" class="customizarConteudo">
+							Outro objetivo a ser destacado é a busca pela inclusão efetiva da criança na escola, não apenas com a criação de condições materiais e de objetos adequados para seu uso, mas também pela construção de uma cultura inclusiva na escola.
+						</p>
+						<p align="justify" class="customizarConteudo">
+ 							O grupo de mediadores reúne-se semanalmente, ocasião em que os casos atendidos por cada profissional são apresentados e as intervenções são discutidas a luz de leituras da área da psicologia do desenvolvimento, da psicomotricidade, da pedagogia, psicopedagogia e do trabalho clínico de acompanhamento terapêutico.
+							A coordenadora do grupo acompanha dando suporte nos casos atendidos e via relatórios semanais caso a caso. Conversa com pais, terapeutas, médicos e escola. 
+							Ocorre também supervisão individual conforme demanda do profissional.
+							Oferecemos mensalmente capacitação para os mediadores, professores e demais interessados na temática.
+						</p>
+						<p align="justify" class="customizarConteudo">
+							<strong>
+									O PAPEL DO MEDIADOR NO PROCESSO DE ENSINO - APRENDIZADO DE ALUNOS ESPECIAIS.
+							</strong>
+							<br/>
+							A função do mediador é sempre de extrema complexidade e merece cuidado constante.
+							A profissão de mediador, como vimos, surge a partir de uma demanda educacional especial nova, que não provém de nenhuma regulamentação no que diz respeito a sua contratação e isso não facilita o processo de Inclusão.
+							Mediadores, hoje, são indispensáveis para a construção de autonomia da escola e do aluno.
+							É esse profissional que demanda todas as necessidades de mudança do processo, sinalizando o que dá certo e o que não dá.
+						<p align="justify" class="customizarConteudo">
+							É ele que possui a relação mais estreita com o aluno especial e sua escuta, se torna fundamental.
+							Porém sua forma de contratação ainda gera muitas dúvidas de conduta à escola.
+							Em muitos momentos, a escola, o professor não se sente confortável com a sua presença, e o sentimento de ameaça pode assombrar, por não sabermos como nos relacionar com ele.
+							Devemos esclarecer em primeiro lugar, que independente de onde venha a sua remuneração, o mediador tem como cliente o aluno.
+							Ele deverá fazer todas as intervenções necessárias junto a escola, à equipe e à família sempre que necessitar.
+						</p>
+						<p align="justify" class="customizarConteudo">
+							Porém alguns acordos são necessários entre mediador e escola, como por exemplo, buscarem sempre saídas juntos para os desafios do cotidiano, antes de acionarem ajuda à família.
+							Por esse motivo, é sempre necessário acolher o profissional de mediação, em suas necessidades básicas de comunicação, não o excluindo, não o delegando todas as responsabilidades.
+							Dialogando sempre, sobre as possíveis soluções, independente de onde provém o seu salário.
+							O mediador deve ser considerado da equipe de trabalho e ele, acolhido em suas demandas.
+							O mediador poderá ser atendido por uma equipe externa para pensar estratégias de trabalho, sempre os acordando com a escola.
+						</p>
+						<p align="justify" class="customizarConteudo">
+							O mediador deverá acionar a família para alguma intervenção, somente em último caso, por esse motivo é vital que a escola se responsabilize pelo aluno, caso o mediador perceba que a escola não se interessa pelo aluno e não colabora para o seu processo, é dever do mediador informar esse aspecto à equipe, de que a família poderá questiona-lo sobre o processo e que não caberá a ele se omitir, tendo em vista seu compromisso com o desenvolvimento do aluno.
+							Nesses casos, gera-se uma relação difícil entre escola e mediação e isso pode se evitar com o comprometimento da escola.
+						</p>
+						<p align="justify" class="customizarConteudo">
+							Quando há envolvimento, a mediação percebe isso, sem precisar cobrar da escola.
+							O papel do mediador em relação ao aluno é descobrir ao tempo do aluno o seu funcionamento e com ele utilizar as ferramentas produzidas pela equipe para incluí-lo.
+							Proporcionar autonomia para que o aluno possa frequentar a escola, sozinho, sem mediação. Portanto, construção constante de autonomia cognitiva e social.
+							É a arte do encontro, que a mediação e aluno terão de descobrir no processo.
+						</p>
+					</header>
+					
+				</article>
+			</div>
+
+			<!-- Equipe -->
+			<div class="wrapper style1">
+				<article id="Equipe">
+					<header>
+						<h2>Equipe</h2>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+									<h3><a href="#">Mônica Farias</a></h3>
+									<p align="justify">Psicóloga, mestre em Psicologia pela UFF, especialista em Psicomotricidade pelo Anthropos.<br> Mediadora escolar, acompanhante terapêuta (Esquizoanalise).<br> Orientadora Educacional seguimento Educação Infantil e Creche ISERJ .</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+									<h3><a href="#">Kamilla Cardoso</a></h3>
+									<p align="justify">pedagoga, especialista em Alfabetização, Leitura e Escrita pela UFRJ, Técnica de Assuntos Educacionais do Colégio Pedro Ii, atuando como orientadora Educacional. Já atuou como professora do primeiro.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+									<h3><a href="#">Mariana Barbosa de Castro</a></h3>
+									<p align="justify">psicóloga pela UFF, atualmente (Junho/2013) fazendo mediação, no Colégio Coração Eucarístico- Flamengo, criança com diagnostico de melingocele.</p>
+								</article>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+									<h3><a href="#">Isabel Vilela</a></h3>
+									<p align="justify">Pedagoga- Mediando criança autista no Colégio Coração Eucarístico- Flamengo.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+									<h3><a href="#">Rafael Mantuano</a></h3>
+									<p align="justify">pedagogo UERJ, mediando no colégio Legrand em Botafogo. ( Em 2014).</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+									<h3><a href="#">   Maria Celeste</a></h3>
+									<p align="justify">Estudante de pedagogia. (Argentina) fazendo mediação no Colégio Oficina do Fazer CEPAENI – Nova Iguaçu (em 2014).</p>
+								</article>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+									<h3><a href="#">Delayne Cunha</a></h3>
+									<p align="justify">estudante de pedagogia, fazendo mediação no colégio Oficina do Fazer-CEPAENI – RJ.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+									<h3><a href="#">Carla Giovanna</a></h3>
+									<p align="justify">Psicóloga, fazendo mediação no colégio Legrand, em Botafogo. </p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+									<h3><a href="#">Elisabete Marques</a></h3>
+									<p align="justify">Professora, Psicopedagoga, fazendo mediação na creche Talento em Laranjeiras.</p>
+								</article>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+									<h3><a href="#">Carolina Reina</a></h3>
+									<p align="justify">Psicopedagoga (Argentina), fazendo mediação no Colégio Municipal Alberto Barth no Flamengo.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+									<h3><a href="#">Tarna Morena</a></h3>
+									<p align="justify">Professora de História e Inglês e Psicopedagoga, fazendo mediação no Colégio Cepaeni Oficina do Fazer em Nova Iguaçu.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+									<h3><a href="#">Juliana Regina</a></h3>
+									<p align="justify">Estudante de psicologia, UFF, mediação colégio Curiosidade.</p>
+								</article>
+							</div>
+						</div>
+					</div>
+					<footer>
+						<a href="#contact" class="button big scrolly">Fale Conosco</a>
+					</footer>
+				</article>
+			</div>
+			<!-- Galeria -->
+			<div class="wrapper style1">
+				<article id="Galeria" class="container small"
+				>
+					<header>
+						<h2>Galeria</h2>
+					</header>
+					<div class="container">
+						<div class="row">
+							<center>
+								<div class="slider-wrapper theme-default">
+						            <div id="slider" class="nivoSlider">
+						                <img src="imagens/IMG_8765.JPG" data-thumb="imagens/IMG_8765.JPG" alt="" />
+						                
+						                <img src="imagens/IMG_8767.JPG" data-thumb="imagens/IMG_8767.JPG" alt="" title="" />
+						                
+						                <img src="imagens/IMG_8775.JPG" data-thumb="imagens/IMG_8775.JPG" alt="" data-transition="slideInLeft" />
+
+						                <img src="imagens/IMG_8824.JPG" data-thumb="imagens/IMG_8824.JPG" alt=""/>
+
+						            </div>
+						        </div>
+							</center>
+						</div>
+					</div>
+					<footer>
+						<a href="#contact" class="button big scrolly">Fale Conosco</a>
+					</footer>
+				</article>
+			</div>
+			<!-- Quem Somos -->
+			<div class="wrapper style2">
+				<article id="quem-somos">
+					<header class="customizarConteudo">
+						<h2>Quem Somos</h2>
+						<p align="justify">Somos um grupo de psicólogos e pedagogos especializados no acompanhamento educacional, social e psicológico de crianças com necessidades educacionais especiais.</p>
+						<p align="justify">
+						Nossa atuação se dá com o acompanhamento lado a lado com a criança, em todos os espaços escolares e sociais por onde ela circula.
+						</p>
+						<p align="justify">
+						Objetivamos desenvolver a capacidade de aprendizagem e de interação afetiva da criança com as demais, a professora e os pais.
+						</p>
+						<p align="justify">
+						Visamos potencializar, também, seu autocuidado, autoestima e fortalecer as bases para o seu desenvolvimento.
+						</p>
+						<p align="justify">
+						Nosso grupo de trabalho reune-se semanalmente, ocasião em que os casos atendidos por cada profissional recebem as contribuições dos demais profissionais.
+						</p>
+					</header>
+					<a href="Intermediar.jpg" target="_blank" class="button big scrolly">Veja Nosso Fouder</a>
+
+				</article>
+			</div>
+
+			<!-- Contact -->
+			<div class="wrapper style4">
+				<article id="contact" class="container small">
+					<header>
+						<h2>Contato</h2>
+						<p>Entre em contato conosco.</p>
+					</header>
+					<div>
+						<div class="row">
+							<div class="12u">
+								<form method="post" action="contact.php">
+									<div>
+										<div class="row">
+											<div class="6u">
+												<input type="text" name="name" id="name" placeholder="Nome" />
+											</div>
+											<div class="6u">
+												<input type="text" name="email" id="email" placeholder="Email" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<input type="text" name="subject" id="subject" placeholder="Assunto" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<textarea name="message" id="message" placeholder="Mensagem"></textarea>
+											</div>
+										</div>
+										<div class="row double">
+											<div class="12u">
+												<ul class="actions">
+													<li><input type="submit" value="Enviar Mensagem" /></li>
+													<li><input type="reset" value="Limpar" class="alt" /></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<hr />
+								<h3>Encontre-me ...</h3>
+								<ul class="social">
+									<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+									<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									<li><a href="#" class="icon fa-tumblr"><span class="label">Tumblr</span></a></li>
+									<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+								</ul>
+								<hr />
+							</div>
+						</div>
+					</div>
+					<footer>
+						<ul id="copyright">
+							<li>&copy; 2014 Todos os direitos reservados.</li><li>Designers: Carlos Eduardo e Charley Oliveira</li>
+						</ul>
+					</footer>
+				</article>
+			</div>
+			
+	</body>
+</html>
+
+<style>
+ .customizarConteudo{
+ 	padding: 20px;
+ }
+</style>
+<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+<script type="text/javascript">
+	$(window).load(function() {
+	    $('#slider').nivoSlider();
+	});
+</script>
